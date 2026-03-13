48a7d10ddf63f3ad2af24088c40484feb89771b9
ChienNguyen <>
2026-03-13 21:39:35 +0700

    chat

--- voice_chat_app/lib/services/text_to_speak.dart
+++ voice_chat_app/lib/services/text_to_speak.dart
@@ -1,8 +1,26 @@
+// import 'package:flutter_tts/flutter_tts.dart';
+
+// FlutterTts tts = FlutterTts();
+
+// Future speak(String text) async {
+//   await tts.setLanguage("vi-VN");
+//   await tts.speak(text);
+// }
+
 import 'package:flutter_tts/flutter_tts.dart';
 
 FlutterTts tts = FlutterTts();
 
 Future speak(String text) async {
+
   await tts.setLanguage("vi-VN");
+
+  await tts.setVoice({
+    "name": "com.apple.voice.compact.vi-VN.Linh",
+    "locale": "vi-VN"
+  });
+
+  await tts.setSpeechRate(0.45);
+
   await tts.speak(text);
 }
